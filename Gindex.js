c37fb56a31b09485541e5a64a97ad44fbe655cc0
abusinessagent-dotcom <abusinessagent@gmail.com>
2026-05-09 17:31:24 +0530

    Update Gindex.js

--- Gindex.js
+++ Gindex.js
@@ -3,6 +3,9 @@ const app = express();
 
 app.use(express.json());
 
+// ✅ Render के लिए dynamic port जरूरी
+const PORT = process.env.PORT || 3000;
+
 app.get("/", (req, res) => {
   res.send("Webhook running ✅");
 });
@@ -12,4 +15,4 @@ app.post("/webhook", (req, res) => {
   res.sendStatus(200);
 });
 
-app.listen(3000, () => console.log("Server running"));
+app.listen(PORT, () => console.log(`Server running on ${PORT}`));
